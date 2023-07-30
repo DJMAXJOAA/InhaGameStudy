3dab28f31f9417d6e1afc693c400d929a9746827
kimhamgu <131941919+kimhamgu@users.noreply.github.com>
2023-07-31 00:57:48 +0900

    [1주차 2번] Reverse Linked List/ - ｍin / https://leetcode.com/problems/reverse-linked-list/

---
+++ 김채연/1주차/1주차 2번.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head) return head;
+
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while (head) {
+            curr = head->next;
+            head->next = prev;
+            prev = head;
+            head = curr;
+        }
+        return prev;
+    }
+};
