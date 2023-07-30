cd3c1d7420d08d565ed9becb7aff6933ebec7b84
DJMAXJOAA <109760354+DJMAXJOAA@users.noreply.github.com>
2023-07-31 01:18:39 +0900

    [1주차 3번] Longest Substring Without Repeating Characters / 1시간20분 / https://leetcode.com/problems/longest-substring-without-repeating-characters/description/ 문제이해를 아예 잘못해서 뻘짓좀 했음 -> 시간은 15퍼 더걸렸는데 메모리는 85퍼 더 덜썼네 다른 풀이방법들 보니 stl 따로 공부좀 해야할거같다

---
+++ 최형준/1주차/230730_1주차 3번.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() == 0)
+            return 0;
+
+        int size = 0;
+        int iMax = 1;
+        vector<pair<bool, int>> temp(128);
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (temp[s[i]].first == true)
+            {
+                int tempi = temp[s[i]].second;
+
+                for (int i = 0; i < temp.size(); i++)
+                {
+                    temp[i].first = false;
+                }
+                temp[s[i]].second = i;
+                i = tempi;
+
+                iMax = max(iMax, size);
+                size = 0;
+
+                continue;
+            }
+            temp[s[i]].first = true;
+            temp[s[i]].second = i;
+            size++;
+        }
+        if (iMax <= size)
+            iMax = size;
+        return iMax;
+    }
+};
