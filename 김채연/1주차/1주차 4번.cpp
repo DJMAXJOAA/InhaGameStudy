285c8aebf347f58912ee46bccc43876885fbfb19
kimhamgu <131941919+kimhamgu@users.noreply.github.com>
2023-07-31 01:00:06 +0900

    [1주차 4번] Longest Substring Without Repeating Characters/min/https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ 김채연/1주차/1주차 4번.cpp
@@ -0,0 +1,15 @@
+const lengthOfLongestSubstring = function(s) {
+    let subStr = "";
+    let maxLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (subStr.includes(s[i])) {
+            subStr = subStr.slice(subStr.indexOf(s[i]) + 1);
+        }
+        subStr += s[i];
+        if (maxLength < subStr.length) {
+            maxLength = subStr.length;
+        }
+    }
+    return maxLength;
+};
