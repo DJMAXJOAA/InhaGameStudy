fc16a82b06dbeaf9db481825305287e1941eda11
Kang-MinJi <87412863+kangminjii@users.noreply.github.com>
2023-07-31 11:52:48 +0900

    1주차 1번 코드 수정+주석

--- 강민지/1주차/1주차 1번.cpp
+++ 강민지/1주차/1주차 1번.cpp
@@ -4,26 +4,28 @@ public:
         
         vector<vector<int>> result;
 
-        int i=0;
-        bool flag=false;
-        int start=newInterval[0];;
-        int end=newInterval[1];
+        int i = 0;
+        int start = newInterval[0];
+        int end = newInterval[1];
 
-        while(i<intervals.size() && intervals[i][1]<newInterval[0])
+        // start가 intervals[i] 범위 안에 없을 때
+        while(i<intervals.size() && intervals[i][1]<start)
         {
-            result.push_back(intervals[i]);
+            result.push_back(intervals[i]); // 통째로 추가
             i++;
         }
 
-        while(i<intervals.size() && intervals[i][0]<=newInterval[1])
+        // interval[i] 값이 newInterval 사이에 있을 때
+        // max, min 갱신
+        while(i<intervals.size() && intervals[i][0]<=end)
         {
-            
             start=min(intervals[i][0],start);
             end=max(intervals[i][1],end);
             i++;
         }
         result.push_back({start,end});
 
+        // newInterval 범위 이후의 interval[i] 값들 추가
         while(i<intervals.size())
         {
             result.push_back(intervals[i]);
