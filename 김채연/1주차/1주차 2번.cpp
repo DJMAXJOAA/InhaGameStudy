53b4bb7c0e19181e9120b4c57bb1d7cf344d15f1
kimhamgu <131941919+kimhamgu@users.noreply.github.com>
2023-07-31 14:09:28 +0900

    주석

--- 김채연/1주차/1주차 2번.cpp
+++ 김채연/1주차/1주차 2번.cpp
@@ -4,8 +4,8 @@ public:
         if (!head) return head;
 
         ListNode* curr = head;
-        ListNode* prev = NULL;
-        while (head) {
+        ListNode* prev = NULL; //마지막 노드의 next는 null이니까.
+        while (head) {//헤드가 있을때까지. =스왑
             curr = head->next;
             head->next = prev;
             prev = head;
--- 김채연/1주차/1주차 4번.cpp
+++ 김채연/1주차/1주차 4번.cpp
@@ -1,15 +1,31 @@
-const lengthOfLongestSubstring = function(s) {
-    let subStr = "";
-    let maxLength = 0;
+//Q4. 주어진 문자열에서 알파벳이 중복되지 않고 
+//    가장 길게 연속되는 문자열 일부를 반환하라
 
-    for (let i = 0; i < s.length; i++) {
-        if (subStr.includes(s[i])) {
-            subStr = subStr.slice(subStr.indexOf(s[i]) + 1);
-        }
-        subStr += s[i];
-        if (maxLength < subStr.length) {
-            maxLength = subStr.length;
-        }
-    }
-    return maxLength;
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		vector<char> Words;
+		int nMax = 0;
+
+		for (int i = 0; i < s.size(); i++)
+		{
+			//진행한 문자 중 중복된 값이 있는지 확인
+			vector<char>::iterator iter = find(Words.begin(), Words.end(), s[i]);
+
+			if (iter != Words.end())		// 있으면 nMax 갱신 및 문자열 제거
+			{
+				if (nMax < Words.size())	nMax = Words.size();
+				Words.erase(Words.begin(), ++iter);	// ??제거는 중복된 문자까지??
+				Words.push_back(s[i]);
+			}
+			else
+			{
+				Words.push_back(s[i]);		// 중복되지 않았으면 문자 추가
+			}
+		}
+		if (nMax < Words.size())	nMax = Words.size();	// 문자열을 모두 탐색한 후 nMax 갱신
+
+		return nMax;
+	}
+
 };
