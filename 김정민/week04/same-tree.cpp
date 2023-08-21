94ff9c6602a517674141ecfa6fd7888ceb356624
jeongmin <extraking@naver.com>
2023-08-21 09:02:00 +0900

    [4주차 3번] Same Tree / https://leetcode.com/problems/same-tree/

---
+++ 김정민/week04/same-tree.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if(p == nullptr || q == nullptr) return false;
+        
+        bool node = (p->val == q->val);
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+
+        return node && left && right;
+    }
+};
