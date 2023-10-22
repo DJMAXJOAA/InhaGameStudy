c2738e99e04e6a9c9af9c3eeed78a64475f72169
Kang-MinJi <87412863+kangminjii@users.noreply.github.com>
2023-10-23 02:10:48 +0900

    [12주차 1번] find-minimum-in-rotated-sorted-array / https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/

---
+++ 강민지/12주차/12주차 1번.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = (start + end) / 2;
+
+        if(nums[start] < nums[end])  
+        {
+            return nums[start];
+        }
+
+        // 이진 검색
+        while(start < end)
+        {
+            if(nums[mid] > nums[end])
+                start = mid + 1;
+            else
+                end = mid;
+            
+             mid = (start + end) / 2;
+        }
+        
+        return nums[start];
+    }
+};
