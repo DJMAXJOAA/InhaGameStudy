be4d50972efb2973d744d8354b54dd9ce50f38a2
kimhamgu <131941919+kimhamgu@users.noreply.github.com>
2023-10-29 22:05:07 +0900

    [13주차 1번] subtree-of-another-tree / min/ https://leetcode.com/problems/subtree-of-another-tree/

--- 김채연/13주차/13주차 1번.cpp
+++ 김채연/13주차/13주차 1번.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    bool isSameTree(TreeNode* p, TreeNode* q) { // 루트가 같은 애들의 자식들까지 같은지 확인.
         if (!p && !q) return true;
         if (!p || !q) return false;
         if (p->val != q->val) return false;
 
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) { //같은 root를 찾고
         if (!root || !subRoot)
             return false;
 
         if (isSameTree(root, subRoot))
-            return true;
+            return true; 
 
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
 
