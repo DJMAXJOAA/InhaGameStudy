b6e2336088b2f530be4f20cf6e7428c087c8f837
rlaoxdn <rlaxodn0215@naver.com>
2023-11-13 18:28:02 +0900

    [15주차 2번] / Container With Most Water / 40분 / https://leetcode.com/problems/container-with-most-water/

---
+++ 김태우/15주차/15주차 2번.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height)
+    {
+        int ans = 0;
+        int start = 0;
+        int end = height.size() - 1;
+
+        while (start <= end)
+        {
+            ans = max(ans, (end - start) * min(height[start], height[end]));
+
+            if (height[start] < height[end])
+            {
+                start++;
+            }
+
+            else
+            {
+                end--;
+            }
+
+        }
+
+        return ans;
+    }
+};
